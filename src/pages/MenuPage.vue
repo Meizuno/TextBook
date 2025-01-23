972fe043d46292f5c435463b44b8f0595cebf1c6
Yurii Myronov <yuramiron16@gmail.com>
2025-01-23 10:23:52 +0100

    add pull to refresh

--- src/pages/MenuPage.vue
+++ src/pages/MenuPage.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="q-pa-md">
-    <q-input dense filled v-model="filter" label="Search" class="q-my-md">
-      <template v-slot:append>
-        <q-icon v-if="filter === ''" name="search" />
-        <q-icon v-else name="clear" class="cursor-pointer" @click="filter = ''" />
-      </template>
-    </q-input>
+  <q-page>
+    <q-pull-to-refresh @refresh="refresh" :bg-color="q.dark.isActive ? 'dark' : 'white'">
+      <div class="q-pa-md">
+        <q-input dense filled v-model="filter" label="Search" class="q-my-md">
+          <template v-slot:append>
+            <q-icon v-if="filter === ''" name="search" />
+            <q-icon v-else name="clear" class="cursor-pointer" @click="filter = ''" />
+          </template>
+        </q-input>
 
-    <q-tree
-      v-bind="{ class: { 'text-center': tree.length === 0 } }"
-      no-nodes-label="No files found"
-      class="text-body1"
-      :nodes="tree"
-      node-key="key"
-      :filter="filter"
-    >
-      <template v-slot:header-folder="prop">
-        <div class="row no-wrap justify-between full-width">
-          <div class="row no-wrap ellipsis line items-center full-width">
-            <q-icon class="q-mr-sm text-blue-8" :name="prop.node.icon" />
-            <div class="full-width">{{ prop.node.label.split('.')[0] }}</div>
-          </div>
-          <div class="row no-wrap">
-            <q-btn flat dense color="primary" icon="edit" @click="editFolder(prop.node)" />
-            <q-btn
-              flat
-              dense
-              color="negative"
-              icon="delete"
-              @click="confirmDeleteNode(prop.node)"
-            />
-          </div>
-        </div>
-      </template>
-      <template v-slot:header-file="prop">
-        <div class="row no-wrap justify-between gap-2 full-width">
-          <div class="row no-wrap ellipsis items-center full-width" @click="chooseFile(prop.node)">
-            <q-icon size="20px" class="q-mr-xs text-blue-8" :name="prop.node.icon" />
-            <div class="flex-1">{{ prop.node.label.split('.')[0] }}</div>
-          </div>
-          <div class="row no-wrap">
-            <q-btn flat dense color="primary" icon="edit" @click="editFile(prop.node)" />
-            <q-btn
-              flat
-              dense
-              color="negative"
-              icon="delete"
-              @click="confirmDeleteNode(prop.node)"
-            />
-          </div>
-        </div>
-      </template>
-    </q-tree>
-  </div>
+        <q-tree
+          v-bind="{ class: { 'text-center': tree.length === 0 } }"
+          no-nodes-label="No files found"
+          class="text-body1"
+          :nodes="tree"
+          node-key="key"
+          :filter="filter"
+        >
+          <template v-slot:header-folder="prop">
+            <div class="row no-wrap justify-between full-width">
+              <div class="row no-wrap ellipsis line items-center full-width">
+                <q-icon class="q-mr-sm text-blue-8" :name="prop.node.icon" />
+                <div class="full-width">{{ prop.node.label.split('.')[0] }}</div>
+              </div>
+              <div class="row no-wrap">
+                <q-btn flat dense color="primary" icon="edit" @click="editFolder(prop.node)" />
+                <q-btn
+                  flat
+                  dense
+                  color="negative"
+                  icon="delete"
+                  @click="confirmDeleteNode(prop.node)"
+                />
+              </div>
+            </div>
+          </template>
+          <template v-slot:header-file="prop">
+            <div class="row no-wrap justify-between gap-2 full-width">
+              <div
+                class="row no-wrap ellipsis items-center full-width"
+                @click="chooseFile(prop.node)"
+              >
+                <q-icon size="20px" class="q-mr-xs text-blue-8" :name="prop.node.icon" />
+                <div class="flex-1">{{ prop.node.label.split('.')[0] }}</div>
+              </div>
+              <div class="row no-wrap">
+                <q-btn flat dense color="primary" icon="edit" @click="editFile(prop.node)" />
+                <q-btn
+                  flat
+                  dense
+                  color="negative"
+                  icon="delete"
+                  @click="confirmDeleteNode(prop.node)"
+                />
+              </div>
+            </div>
+          </template>
+        </q-tree>
+      </div>
+    </q-pull-to-refresh>
+  </q-page>
 </template>
 
 <script setup lang="ts">
@@ -70,10 +77,16 @@ const q = useQuasar()
 const { setActiveNode, setSelectedNode } = useNodeStore()
 const { deleteNode } = useNode()
 const { tree } = storeToRefs(useTreeStore())
+const { setTree } = useTreeStore()
 const { error } = useNotify()
 const { navigate } = useNavigation()
 
 const filter = ref('')
+
+const refresh = async (done: () => void) => {
+  await setTree()
+  done()
+}
 
 const chooseFile = async (node: TreeNode) => {
   setActiveNode(node)
